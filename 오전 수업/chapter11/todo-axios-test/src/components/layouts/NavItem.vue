74bbd334250163cf086a8b29a43d8f6d00262324
Hyejin Kim <chol159156@gmail.com>
2025-04-02 17:46:42 +0900

    2025.04.02 실습

---
+++ 오전 수업/chapter11/todo-axios-test/src/components/layouts/NavItem.vue
@@ -0,0 +1,15 @@
+<!-- NavItem 컴포넌트 -->
+<template>
+  <li class="nav-item">
+    <RouterLink :to="menu.uri" class="nav-link">{{ menu.text }}</RouterLink>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  menu: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
---
+++ 오전 수업/chapter11/todo-axios-test/src/pages/All.vue
@@ -0,0 +1,3 @@
+<template><h1>All</h1></template>
+
+<script setup></script>
